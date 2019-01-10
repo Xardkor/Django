53949fe2ee4be246f2ad6cc4e3971ad2162029dd
Xardkor <raynorraiders2@gmail.com>
2019-01-10 20:15:56 +0200

    End of func blog

--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -1,24 +1,39 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.shortcuts import redirect
+from django.urls import reverse
 from django.views.generic import View
 from .models import Post, Tag
-from .utils import ObjectDetailMixin, ObjectCreateMixin
+from .utils import *
 from .forms import TagForm, PostForm
 
+
 def posts_list(requests):
+
     posts = Post.objects.all()
     return render(requests, 'blog/index.html', context={'posts': posts})
 
 
 class PostDetail(ObjectDetailMixin, View):
+
     model = Post
     template = 'blog/post_detail.html'
 
+
 class PostCreate(ObjectCreateMixin, View):
+
     form_model = PostForm
     template = 'blog/post_create_form.html'
 
+class PostUpdate(ObjectUpdateMixin, View):
+    model = Post
+    model_form = PostForm
+    template = 'blog/post_update_form.html'
+
+class PostDelete(ObjectDeleteMixin, View):
+    model = Post
+    template = 'blog/post_delete_form.html'
+    redirect_url = 'posts_list_url'
 
 
 def tags_list(request):
@@ -27,13 +42,32 @@ def tags_list(request):
 
 
 class TagDetail(ObjectDetailMixin, View):
+
     model = Tag
     template = 'blog/tag_detail.html'
 
+class TagUpdate(ObjectUpdateMixin, View):
+    model = Tag
+    model_form = TagForm
+    template = 'blog/tag_update_form.html'
+
+class TagDelete(ObjectDeleteMixin, View):
+    model = Tag
+    template = 'blog/tag_delete_form.html'
+    redirect_url = 'tags_list_url'
+    # def get(self, request, slug):
+    #     tag = Tag.objects.get(slug__iexact=slug)
+    #     return render(request, 'blog/tag_delete_form.html', context={'tag': tag})
+    #
+    # def post(self, request, slug):
+    #     tag = Tag.objects.get(slug__iexact=slug)
+    #     tag.delete()
+    #     return redirect(reverse('tags_list_url'))
+
 
 class TagCreate(ObjectCreateMixin, View):
+
     form_model = TagForm
     template = 'blog/tag_create.html'
-
 
 
